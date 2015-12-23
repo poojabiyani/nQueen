fdeb17c124be5bc409825a7e65a1aead9ff95e84
Pushkal17 <pushkalagarwal@gmail.com>
2015-12-24 02:53:19 +0530

    just for testing.

--- UITEST/src/myUI.java
+++ UITEST/src/myUI.java
@@ -1,11 +1,18 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class myUI {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -38,6 +45,48 @@ public class myUI {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textField.setText("1");
+			}
+		});
+		btnNewButton.setBounds(54, 66, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+				textField.setText("3");
+				}
+		});
+		btnNewButton_1.setBounds(54, 91, 89, 23);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText("2");
+			}
+		});
+		btnNewButton_2.setBounds(142, 66, 89, 23);
+		frame.getContentPane().add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText("4");
+			}
+		});
+		btnNewButton_3.setBounds(142, 91, 89, 23);
+		frame.getContentPane().add(btnNewButton_3);
+		
+		textField = new JTextField();
+		textField.setBounds(56, 167, 200, 50);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
 	}
-
 }
